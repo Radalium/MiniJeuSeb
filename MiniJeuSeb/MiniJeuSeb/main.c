0bb070bce5fb798ca4b87d2310d8e71a4d7cf511
Valette Evan <valetteevan23@gmail.com>
2023-12-18 13:46:14 +0100

    add tools

--- MiniJeuSeb/MiniJeuSeb/main.c
+++ MiniJeuSeb/MiniJeuSeb/main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "tools.h"
 #include "SFML/Graphics.h"
 
 int main() {
 
-		
-
-
+	
 }
--- MiniJeuSeb/MiniJeuSeb/tools.h
+++ MiniJeuSeb/MiniJeuSeb/tools.h
@@ -1,1 +1,10 @@
 #pragma once
+#include "SFML/Graphics.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+void initTools();
+void restartClock();
+float GetDeltaTime();
